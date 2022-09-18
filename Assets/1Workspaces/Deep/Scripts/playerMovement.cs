993c45479f6deb0576650c034137d33e86ae8849
dcpais <dpaismail@gmail.com>
2022-09-18 13:59:05 +1000

    Added player VR and player PC, XR plugins odnwloaded

--- Assets/1Workspaces/Deep/Scripts/playerMovement.cs
+++ Assets/1Workspaces/Deep/Scripts/playerMovement.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -19.6f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +27,25 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z; 
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
